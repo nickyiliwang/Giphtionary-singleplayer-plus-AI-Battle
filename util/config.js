4010c0a59d43a80432e4c6ef57532da819b9e4ab
Nick Wang <nickyiliwang@gmail.com>
2019-10-18 00:48:45 -0400

    added config file

---
+++ util/config.js
@@ -0,0 +1,7 @@
+const instanceLocator = "v1:us1:cd742369-0fc0-4252-b21d-d41cf7d58444";
+const secreteKey =
+  "cdbd8b87-fa0f-4374-94e9-7e6a21db9f0d:t5o0nvy8lfgRc5etenacyB+WZBq76XbN85lx0F+Kh64=";
+const TokenProviderToken =
+  "https://us1.pusherplatform.io/services/chatkit_token_provider/v1/cd742369-0fc0-4252-b21d-d41cf7d58444/token";
+
+const GiphyApiKey = "ZvjarsBhTt4QjlDiu1aeTNVMZoMa6fVt";
